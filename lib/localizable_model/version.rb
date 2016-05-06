c4d29cdeb35308ba7974c4752c095f7be7f025e4
Inge Jørgensen <inge@manualdesign.no>
2016-05-06 16:34:18 +0200

    Version 0.0.1

--- lib/localizable_model/version.rb
+++ lib/localizable_model/version.rb
@@ -1,5 +1,5 @@
 # encoding: utf-8
 
 module LocalizableModel
-  VERSION = "0.5.0".freeze unless LocalizableModel.const_defined?("VERSION")
+  VERSION = "0.0.1".freeze unless LocalizableModel.const_defined?("VERSION")
 end
