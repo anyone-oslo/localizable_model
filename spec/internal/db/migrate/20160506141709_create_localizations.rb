12c2af1aceaab1a46e0b61bc4b3150923b4f2f39
Inge Jørgensen <inge@anyone.no>
2017-06-07 17:26:23 +0200

    Fix test migrations

--- spec/internal/db/migrate/20160506141709_create_localizations.rb
+++ spec/internal/db/migrate/20160506141709_create_localizations.rb
@@ -1,4 +1,4 @@
-class CreateLocalizations < ActiveRecord::Migration
+class CreateLocalizations < ActiveRecord::Migration[4.2]
   def change
     create_table :localizations do |t|
       t.references :localizable, polymorphic: true
