edf781e22b071ab9b2f3706ae3a11023b7e24105
Inge Jørgensen <inge@anyone.no>
2017-08-23 02:41:52 +0200

    Bump version to 0.6.0

--- lib/localizable_model/version.rb
+++ lib/localizable_model/version.rb
@@ -1,5 +1,5 @@
 # encoding: utf-8
 
 module LocalizableModel
-  VERSION = "0.5.5".freeze unless LocalizableModel.const_defined?("VERSION")
+  VERSION = "0.6.0".freeze unless LocalizableModel.const_defined?("VERSION")
 end
