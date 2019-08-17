eb22220338267beb624c013645062411042fb0d5
Inge Jørgensen <inge@anyone.no>
2019-08-17 03:48:40 +0200

    Version 0.6.4

--- lib/localizable_model/version.rb
+++ lib/localizable_model/version.rb
@@ -1,5 +1,5 @@
 # encoding: utf-8
 
 module LocalizableModel
-  VERSION = "0.6.3".freeze unless LocalizableModel.const_defined?("VERSION")
+  VERSION = "0.6.4".freeze unless LocalizableModel.const_defined?("VERSION")
 end
