2b05a5e3b47f487318b4482e7cebf1f049268817
Inge Jørgensen <inge@elektronaut.no>
2026-06-23 19:39:45 +0200

    chore(main): release localizable_model 0.6.8

--- lib/localizable_model/version.rb
+++ lib/localizable_model/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module LocalizableModel
-  VERSION = "0.6.7" unless LocalizableModel.const_defined?("VERSION")
+  VERSION = "0.6.8" unless LocalizableModel.const_defined?("VERSION")
 end
